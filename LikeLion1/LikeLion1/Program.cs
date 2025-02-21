cea247bc2e15008d40f5ff0cf61acacd40274872
bogeonK <130254026+bogeonK@users.noreply.github.com>
2025-02-21 09:57:41 +0900

    study1
    Posting to git

---
+++ LikeLion1/LikeLion1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion1
+{
+    class Program
+    {
+        // 출력
+        static void Main(string[] args)
+        {
+            Console.WriteLine("깃허브 연결하기");
+        }
+    }
+}
