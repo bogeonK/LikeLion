b658fef9ced529b3f10e8bde8e2e0ad102df8752
bogeonK <130254026+bogeonK@users.noreply.github.com>
2025-02-21 10:42:12 +0900

    study3(literal)

---
+++ study3/study3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 리터럴 : 코드에서 고정된 값
+            int hp = 100; // hp
+            double att = 56.7; // att
+            char rank = 'S'; // 등급
+            string name = "Alice"; // 캐릭터 이름
+
+            Console.WriteLine(hp); // 출력 : 100
+            Console.WriteLine(att);      // 출력 : 56.7
+            Console.WriteLine(rank);      // 출력 : S
+            Console.WriteLine(name);      // 출력 : Alice
+        }   
+    }
+}
