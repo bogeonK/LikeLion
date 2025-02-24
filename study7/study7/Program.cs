6dd80a2afbf3661d2982650afdf5dd475f986984
bogeonK <130254026+bogeonK@users.noreply.github.com>
2025-02-24 11:57:47 +0900

    2day

---
+++ study7/study7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10, b = 3;
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+
+            ////문자열 연결 연산자
+            //string firstName = "Alice";
+            //string lastName = "Smith";
+
+            //Console.WriteLine(firstName + " " + lastName); // 출력
+
+            //Console.WriteLine("국어 점수를 입력하세요: ");
+            //int korLan = int.Parse(Console.ReadLine());
+            //Console.WriteLine("영어 점수를 입력하세요: ");
+            //int eng = int.Parse(Console.ReadLine());
+            //Console.WriteLine("수학 점수를 입력하세요: ");
+            //int math = int.Parse(Console.ReadLine());
+
+            //int sum = korLan + eng + math;
+            //float E = (float)sum / 3;
+
+            //Console.WriteLine($"총점 : {sum}, 평균 : {E:F2}");
+
+
+            ////문제2
+            //Console.WriteLine("정수를 입력하세요: ");
+            //int num = int.Parse(Console.ReadLine());
+            //int revNum = ~num;
+
+            //Console.WriteLine($"원래의 값:{num}, 비트 반전 후 값:{revNum}");
+
+        }
+    }
+}
