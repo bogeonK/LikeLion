cf31b1ea2d3f61669737c175cc03065433873914
bogeonK <130254026+bogeonK@users.noreply.github.com>
2025-02-28 16:08:09 +0900

    day6

---
+++ 6day/study20/study20/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study20
+{
+    // 마린 클래스 만들기
+    //이름, 미네랄 = 50
+    //기본생성자, 인자있는 생성자
+
+    class Marin
+    {
+        public string Name;
+        public int Mineral;
+        public Marin() //기본 생성자
+        {
+            Name = "마린";
+            Mineral = 50;
+        }
+        public Marin(string _name, int _mineral) //인자있는 생성자
+        {
+            Name = _name;
+            Mineral = _mineral;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름 : {Name}, 미네랄 : {Mineral}");
+        }
+    }
+
+    // SCV 클래스 만들기
+    //이름, 미네랄 = 50
+    //기본생성자, 인자있는 생성자
+
+    class SCV
+    {
+        public string Name;
+        public int Mineral;
+        public SCV() //기본 생성자
+        {
+            Name = "SCV";
+            Mineral = 50;
+        }
+        public SCV(string _name, int _mineral) //인자있는 생성자
+        {
+            Name = _name;
+            Mineral = _mineral;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름 : {Name}, 미네랄 : {Mineral}");
+        }
+    }
+
+    //배럭클래스를 만드세요
+    //Barrack 150
+    //this 키워드를 이용해보자
+    //this 자기자신을 가르킴
+    class Barrack
+    {
+        public string Name;
+        public int Mineral;
+
+        public Barrack()
+        {
+            Name = "Barrack";
+            Mineral = 150;
+        }
+
+        public Barrack(string Name, int Mineral) { 
+            this.Name = Name;
+            this.Mineral = Mineral;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"건물이름 : {Name}  |  비용 : {Mineral}");
+        }
+    }
+
+    //미네랄 클래스를 만드세요
+    //Mineral 1500 기본으로 줌
+    //7개가 시작부터 있음
+    //클래스화
+
+    //class Mineral
+    //{
+    //    public string Name;
+    //    public int Basic;
+
+    //    public Mineral()
+    //    {
+    //        Name = "미네랄";
+    //        Basic = 1500;
+            
+    //    }
+
+    //    public Mineral(string Name, int Basic)
+    //    {
+    //        this.Name = Name;
+    //        this.Basic = Basic;
+    //    }
+
+    //    public void ShowInfo()
+    //    {
+    //        Console.WriteLine($"현재 미네랄 양 : {Basic}");
+    //    }
+    //}
+
+    //class Person
+    //{
+    //    public string Name;
+    //    public int Age;
+
+    //    //기본 생성자
+    //    //클래스가 객체로 생성될때 자동으로 실행되는 특별한 메서드
+    //    //클래스와 같은 이름가지며, 반환형이 없음(void도 사용하지않음)
+    //    //객체를 만들때 필요한 초기값을 설정할때 사용을 많이 한다.
+
+    //    public Person(string name, int age)
+    //    {
+    //        Name = name;
+    //        Age = age;
+    //        Console.WriteLine("매개변수있는 생성자가 실행됨");
+    //    }
+
+    //    public void ShowInfo()
+    //    {
+    //        Console.WriteLine($"이름 : {Name}   | 나이 : {Age}");
+    //    }
+    //}
+
+    
+    // Game 클래스를 만들어보자
+    class Game
+    {
+        public static int mineral;
+        public static int gas;
+        public static int charCount;
+
+        public static void ShowInfo()
+        {
+            Console.WriteLine($"미네랄 : {mineral}  |  가스 : {gas}  |  인구수: {charCount}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 클래스
+            //
+            //Person p1 = new Person("이름없음", 12); //객체 생성       instance
+            //p1.ShowInfo();
+
+            //Marin marin = new Marin();
+            //SCV scv = new SCV();
+
+            //Marin marin = new Marin("불꽃 테란", 50);
+            //SCV scv = new SCV("일꾼", 50);
+
+            //marin.ShowInfo();
+            //scv.ShowInfo();
+
+            //Barrack barrack = new Barrack();
+            //barrack.ShowInfo();
+
+            ////클래스의 배열
+            //Mineral[] mineral = new Mineral[7];
+            //for(int i=0; i<mineral.Length; i++)
+            //{
+            //    mineral[i] = new Mineral();
+            //    mineral[i].ShowInfo();
+            //}
+
+            Game.mineral = 50;
+            Game.gas = 0;
+            Game.charCount = 4;
+            Game.ShowInfo();
+
+
+
+
+        }
+    }
+}
---
+++ 6day/study21/study21/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace study21
+{
+    //class Person
+    //{
+    //    private string name; // 내부 변수 (main함수에서 쓸 수 없음)
+
+    //    //값 설정하기(Setter)
+    //    public void SetName(string newName)
+    //    {
+    //        name = newName;
+    //    }
+
+    //    //값 가져오기 (Getter)
+    //    public string GetName()
+    //    {
+    //        return name;
+    //    }
+
+    //}
+
+    //class Person
+    //{
+    //    private string name; // 내부 변수 (main함수에서 쓸 수 없음)
+
+    //    public string Name // 프로퍼티
+    //    {
+    //        get { return name; } // Getter
+    //        set { name = value; } // Setter
+    //    }
+
+    //}
+
+
+    ////프로퍼티 자동 구현
+    //class Person
+    //{
+    //    private int count = 100;
+    //    public string Name { get; set; }    // 자동 구현 프로퍼티
+
+    //    public int Count
+    //    {
+    //        get { return count; } // 읽기만 가능
+    //    }
+
+    //    public float Balance { get; private set; } //외부 변경 불가
+
+    //    public void AddBal()
+    //    {
+    //        Balance += 100;
+    //    }
+    //}
+
+    //프로퍼티 자동 구현
+    class Marin
+    {
+        public string Name { get; set; }    // 자동 구현 프로퍼티
+        public int Mineral { get; set; }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //c#에서 get/set 방식의 함수와 프로퍼티 비교
+            //C#에서는 객체의 값을 읽고(get), 설정(set)하는
+            //방식으로 함수(get/set 메서드)또는
+            //**프로퍼티(Property)**를 사용할 수 있음
+            Marin p = new Marin();
+            p.Name= "마린";
+            p.Mineral = 50;
+            
+            Console.WriteLine("이름 : " + p.Name + " 미네랄 : " + p.Mineral);
+        }
+    }
+}
